332cc64c89687adeefe756b201f28091c9b14850
Maykell Carrillo <maykellcarrillo@gmail.com>
2021-09-18 23:22:51 -0300

    Agrega header y fix que evita el oom

--- components/atoms/NavigationItemDesktop.tsx
+++ components/atoms/NavigationItemDesktop.tsx
@@ -1,11 +1,19 @@
+import { FC } from 'react'
 
-const NavigationItemDesktop = () => {
+const NavigationItemDesktop: FC<NavigationItemDesktop> = ({ className = '', href, text, active = false }) => {
   return (
-    <div className="relative z-10">
-      <a href="#home">Home</a>
-      <span className="title-bg bg-primary"></span>
+    <div className={`${className} relative z-10`}>
+      <a className="anchor" href={href}>{text}</a>
+      {active && <span className="title-bg bg-primary"></span>}
     </div>
   )
+}
+
+type NavigationItemDesktop = {
+  className?: string
+  href: string
+  text: string
+  active?: boolean
 }
 
 export default NavigationItemDesktop
